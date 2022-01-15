63e8a7801e22e72e78d45923d4279e36d37e1062
Vinh Le Duy <vinhduyle@Vinhs-MacBook-Pro.local>
2022-01-14 17:08:28 -0800

    first commit

---
+++ src/main/java/edu/usfca/vinh/App.java
@@ -0,0 +1,13 @@
+package edu.usfca.vinh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
